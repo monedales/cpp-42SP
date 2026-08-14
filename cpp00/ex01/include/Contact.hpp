fe69c77c27d0bbec8080d0473ec1edaf4b28681a
Letícia Moneda <le.moneda@outlook.com>
2026-08-14 17:42:12 -0300

    🚧 wip: ex01

---
+++ cpp00/ex01/include/Contact.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Contact
+{
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string nickName;
+        std::string phone;
+        std::string darkestSecret;
+    public:
+        Contact();
+        ~Contact();
+};
---
+++ cpp00/ex01/include/PhoneBook.hpp
@@ -0,0 +1,8 @@
+#include "Contact.hpp"
+
+class PhoneBook
+{
+    private:
+        Contact contacts[8];
+    public:
+};
