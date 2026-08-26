de4b270cd3f206e8cded960cfd44164ace4113ac
monedales <le.moneda@outlook.com>
2026-08-26 16:05:30 -0300

    fix: update Makefile to reference correct source file and remove main.cpp

--- cpp00/ex00/Megaphone.cpp
+++ cpp00/ex00/Megaphone.cpp
@@ -1,26 +1,39 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <cctype>
 
-class Megaphone 
+class Megaphone
 {
     public:
         Megaphone() {}
         ~Megaphone() {}
 
-    void stdoutNoise()
-    {
-       std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    }
-
-    void stdoutDynamic(std::string words)
-    {
-        char ch;
-
-        for (size_t i = 0; i < words.length(); i++)
+        void stdoutNoise()
         {
-            ch = static_cast<char>(std::toupper(static_cast<unsigned char>(words[i])));    
-            std::cout << ch;
+            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         }
-    }
+
+        void stdoutDynamic(std::string words)
+        {
+            for (size_t i = 0; i < words.length(); i++)
+            {
+                std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(words[i])));
+            }
+        }
 };
+
+int main(int argc, char **argv)
+{
+    Megaphone megaphone;
+
+    if (argc == 1)
+    {
+        megaphone.stdoutNoise();
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+        megaphone.stdoutDynamic(argv[i]);
+    std::cout << std::endl;
+
+    return (0);
+}
