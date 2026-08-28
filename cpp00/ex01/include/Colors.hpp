4693a928db605ab1ae688aec44124135fea05af4
monedales <le.moneda@outlook.com>
2026-08-28 16:48:03 -0300

    feat: add Colors.hpp for color constants in terminal output

---
+++ cpp00/ex01/include/Colors.hpp
@@ -0,0 +1,15 @@
+#ifndef COLORS_H
+#define COLORS_H
+#include <string>
+
+const std::string RESET   = "\033[0m";
+const std::string BOLD    = "\033[1m";
+const std::string LILAC   = "\033[38;2;200;162;200m";
+const std::string PINK    = "\033[38;2;255;105;180m";
+const std::string MINT    = "\033[38;2;150;255;200m";
+const std::string PEACH   = "\033[38;2;255;190;150m";
+const std::string CYAN    = "\033[38;2;120;220;255m";
+const std::string RED     = "\033[38;2;255;80;80m";
+const std::string MAGENTA = "\033[38;2;255;80;220m";
+
+#endif
