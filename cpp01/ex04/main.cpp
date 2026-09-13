00dacb0ce7227c74b969e352825a76ca17b6fb59
monedales <le.moneda@outlook.com>
2026-09-13 13:06:47 -0300

    ✨ feat: extract replace logic into separate function for improved readability and reusability

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,6 +3,18 @@
 #include <string>
 
 
+void replaceAll(std::string &line, const std::string &s1, const std::string &s2)
+{
+	size_t pos = line.find(s1, 0);
+	while (pos != std::string::npos)
+	{
+		std::string before = line.substr(0, pos);
+		std::string after = line.substr(pos + s1.length());
+		line = before + s2 + after;
+		pos = line.find(s1, pos + s2.length());
+	}
+}
+
 int main(int argc, char **argv) 
 {
 	if (argc != 4 || !argv[2][0] || !argv[3][0])
@@ -25,14 +37,7 @@ int main(int argc, char **argv)
 	std::string line;
 	while (std::getline(file_in, line))
 	{
-		size_t pos = line.find(s1, 0);
-		while (pos != std::string::npos) 
-		{
-			std::string before = line.substr(0, pos);
-			std::string after = line.substr(pos + s1.length());
-			line = before + s2 + after;
-			pos = line.find(s1, pos + s2.length());
-		}
+		replaceAll(line, s1, s2);
 		file_out << line << std::endl;
 	}
 	file_in.close();
